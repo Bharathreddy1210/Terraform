8b3dcc35bd39575fee1c36f7e60de1095a22967a
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-24 11:19:02 +0200

    s3 bucket

--- s3-state/s3-state.tf
+++ s3-state/s3-state.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0089b8e98cd95257d"
+  ami                    = ["ami-0089b8e98cd95257d"]
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
   tags = {
