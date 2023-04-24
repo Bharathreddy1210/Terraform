fdfb5369eb789c4664bb44f604c47602babb1cdf
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-24 11:13:49 +0200

    s3 bucket

--- s3-state/s3-state.tf
+++ s3-state/s3-state.tf
@@ -11,6 +11,6 @@ resource "aws_instance" "ec2" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
   tags = {
-    Name = "test"
+    Name = "test-1"
   }
 }
