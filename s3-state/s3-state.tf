1ac349a88696d1f93b2510c9bd1378ff4c3ca5a8
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-24 11:07:12 +0200

    s3 bucket

---
+++ s3-state/s3-state.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "devopspractice-6"
+    key = "s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
+  tags = {
+    Name = "test"
+  }
+}
