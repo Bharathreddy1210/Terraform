594e18615ee6281f8a9b219ff109e3b5116cd0ff
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-24 11:24:59 +0200

    s3 bucket

--- Sample/text.tf
+++ Sample/text.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0089b8e98cd95257d"
+  ami                    = "ami-0b5a2b5b8f2be4ec2"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
   tags = {
