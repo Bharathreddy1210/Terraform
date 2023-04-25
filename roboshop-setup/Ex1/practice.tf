7444fa5661dde88935a39790569985c2ca3f1733
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-25 09:32:25 +0200

    count

--- roboshop-setup/Ex1/practice.tf
+++ roboshop-setup/Ex1/practice.tf
@@ -8,5 +8,5 @@ resource "aws_instance" "frontend" {
   count = 5
   ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_securtiy_group_ids = ["sg-0c8964a7a7b04adc0"]
+  vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
 }
