348490438cd05b0c18f1262dfc02f91165d5b5e5
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-25 09:38:55 +0200

    count

--- roboshop-setup/Ex1/practice.tf
+++ roboshop-setup/Ex1/practice.tf
@@ -1,12 +1,23 @@
-data "aws_ami" "ami"  {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["631186618481"]
+  owners      = ["973714476881"]
 }
 
 resource "aws_instance" "frontend" {
-  count = 5
-  ami = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+variable "instances" {
+  default = ["cart", "catalogue", "user", "payment", "shipping"]
+}
+
+output "public-ip" {
+  value = aws_instance.frontend.*.public_ip
 }
