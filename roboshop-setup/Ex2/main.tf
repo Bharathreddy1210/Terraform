2b1bae7e56a2a854e5ae0b79d5d77f10e45ce336
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-25 10:36:14 +0200

    structure of code

--- roboshop-setup/Ex2/main.tf
+++ roboshop-setup/Ex2/main.tf
@@ -18,11 +18,11 @@ resource "aws_instance" "frontend" {
 variable "instances" {
   default = {
     catalogue = {
-      name = catalogue
+      name = "catalogue"
       type = "t3.micro"
     }
     user = {
-      name = user
+      name = "user"
       type = "t3.small"
     }
   }
