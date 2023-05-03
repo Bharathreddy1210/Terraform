516c53dd4fba7b8959ae997bacb3dba29d4b3ea6
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-05-03 11:34:23 +0200

    Modules in roboshop

---
+++ roboshop-setup/EX3/ec2/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z04815523BC94LQKLB87B"
+  name    = "roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
