8d29e18204d02d727bb7049364c78629c3e94488
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-05-03 10:40:47 +0200

    Conditions

--- conditions/main.tf
+++ conditions/main.tf
@@ -3,4 +3,12 @@ resource "aws_instance" "test" {
   instance_type = var.instance_type == "" ? "t3.micro" : "instance_type"
 }
 
+variable "create_instances" {}
+
+resource "aws_instance" "test2" {
+  count = var.create_instances == "true" ? 1 : 0
+  ami = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = var.instance_type == "" ? "t3.micro" : "instance_type"
+}
+
 variable "instance_type" {}
