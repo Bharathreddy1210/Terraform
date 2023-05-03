0611d58c4e4da433ad48094d81b96fdbc022f9f8
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-05-03 10:45:36 +0200

    Conditions string to boolean

--- conditions/main.tf
+++ conditions/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test" {
 variable "create_instances" {}
 
 resource "aws_instance" "test2" {
-  count = var.create_instances == "true" ? 1 : 0
+  count = tobool(var.create_instances) ? 1 : 0
   ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = var.instance_type == "" ? "t3.micro" : "instance_type"
 }
