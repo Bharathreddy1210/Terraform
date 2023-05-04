bcc11bd610c236c1e48b013a76e6ef0a615154b1
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-05-04 17:27:42 +0200

    provisioner

--- provisioner/main.tf
+++ provisioner/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0b5a2b5b8f2be4ec2"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-0c8964a7a7b04adc0]
+  vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
   tags = {
     Name = "demo"
   }
