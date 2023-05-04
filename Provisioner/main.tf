fda28b6ec0cea456a553a6e78262a70daf5e4a13
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-05-04 17:13:00 +0200

    provisioner

---
+++ Provisioner/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "ec2" {
+  ami                    ="ami-0b5a2b5b8f2be4ec2"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0c8964a7a7b04adc0"]
+  tags = {
+    Name = "demo"
+  }
+}
+
+provisioner "remote-exec" {
+
+  connection {
+    host     = self.public_ip
+    user     = "root"
+    password = "DevOps321"
+  }
+
+  inline = [
+    "echo Hello"
+  ]
+}
+
+}
+
+
